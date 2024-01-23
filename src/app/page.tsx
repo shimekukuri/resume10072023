b831fb5e1281e3530a29356d7e28ddff17f42b23
Tyler Hutchinson <jhutchinson192390@gmail.com>
2024-01-23 16:54:59 -0500

    More spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <main className="flex-1 overflow-y-clip overflow-x-scroll snap-mandatory snap-x flex flex-row lg:gap-2 max-w-[100vw] md:min-h-full">
       <div className="min-w-[10vw] min-h-[100%] hidden md:block"></div>
-      <div className="pb-14 pt-1 pl-1 pr-1 lg:p-0 min-w-[100vw] lg:min-w-[80vw] grid grid-cols-3 lg:grid-cols-4 grid-rows-4 lg:grid-rows-3 content-center gap-1 md:gap-2 snap-center">
+      <div className="pb-14 pt-14 pl-1 pr-1 lg:p-0 lg:pb-0 lg:pt-0 lg:pl-0 lg:pr-0 min-w-[100vw] lg:min-w-[80vw] grid grid-cols-3 lg:grid-cols-4 grid-rows-4 lg:grid-rows-3 content-center gap-1 md:gap-2 snap-center">
         <Hero />
         <Channel href="#my_modal_1" />
         <Channel href="/test" />
