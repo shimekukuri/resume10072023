1278b4e8a2f09e870bd35c377392544b75fc2b11
Tyler Hutchinson <jhutchinson192390@gmail.com>
2024-01-23 16:52:30 -0500

    Testing spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Hero from "@/components/hero/Hero";
 
 export default function Page() {
   return (
-    <main className="flex-1 overflow-y-clip overflow-x-scroll snap-mandatory snap-x flex flex-row lg:gap-2 max-w-[100vw] min-h-full">
+    <main className="flex-1 overflow-y-clip overflow-x-scroll snap-mandatory snap-x flex flex-row lg:gap-2 max-w-[100vw] md:min-h-full">
       <div className="min-w-[10vw] min-h-[100%] hidden md:block"></div>
-      <div className="p-4 lg:p-0 min-w-[100vw] lg:min-w-[80vw] grid grid-cols-3 lg:grid-cols-4 grid-rows-4 lg:grid-rows-3 content-center gap-2 snap-center">
+      <div className="pb-14 pt-1 pl-1 pr-1 lg:p-0 min-w-[100vw] lg:min-w-[80vw] grid grid-cols-3 lg:grid-cols-4 grid-rows-4 lg:grid-rows-3 content-center gap-1 md:gap-2 snap-center">
         <Hero />
         <Channel href="#my_modal_1" />
         <Channel href="/test" />
